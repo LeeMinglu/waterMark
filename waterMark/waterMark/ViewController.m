921c399794cb44f9ca9c96101ec75e2351140701
LeeMinglu <402007353@qq.com>
2016-01-11 21:28:54 +0800

    为图像添加水印

--- waterMark/waterMark/ViewController.m
+++ waterMark/waterMark/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+WaterMark.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *baseImage = [UIImage imageNamed:@"IMG_0821.JPG"];
+    
+//    UIImage *resultImage = [baseImage imageWaterMarkWithString:@"我是rock" point:CGPointMake(20, 200) attribute:@{NSForegroundColorAttributeName:[UIColor blueColor]} image:[UIImage imageNamed:@"1"] imagePoint:CGPointMake(0, 0) alpha:0.2];
+    
+    UIImage *resultImage = [baseImage imageWaterMarkWithString:@"我是rock" rect:CGRectMake(0, 100, 200, 100) attribute:@{NSForegroundColorAttributeName:[UIColor blueColor]} image:[UIImage imageNamed:@"1"]  imageRect:CGRectMake(0, 0, self.view.bounds.size.width, 100) alpha:0.4];
+    
+    UIImageView  *imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
+    
+    imageView.image = resultImage;
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    [self.view addSubview:imageView];
+    
 }
 
 - (void)didReceiveMemoryWarning {
